e050b6b11de9e1484dc292a908a0f5dd0975e185
Aleksei Ponyatov <ponyatov94@gmail.com>
2024-10-04 20:52:32 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	fmt.Println("Start work on my blog")
+
+	router := gin.Default()
+	router.Static("/static", "./web/static")
+	router.LoadHTMLGlob("./web/templates/*")
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	router.Run("localhost:8686")
+}
